b73194f065bcb38384b8eedd992b7c797cf9d8f8
lmadhuranga <lilan.maduranga@gmail.com>
2018-12-23 18:52:07 +0530

    basic strucure ok

--- dns resolve and ip.lua
+++
@@ -1,3 +0,0 @@
-sk = net.createConnection(net.TCP, 0)
-sk:dns("www.nodemcu.com", function(conn, ip) print(ip) end)
-sk = nil
--- init_next.lua
+++
@@ -1,6 +0,0 @@
--- -- One time ESP Setup --
-wifi.setmode(wifi.STATION)
-wifi.sta.config ("mad-host","madhost1")  
-print(wifi.sta.getip())
-
-dofile("next.lua")
