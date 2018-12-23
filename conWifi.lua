05fae377cc95ed50f3a46c3fe76a51bb8ef738c8
lmadhuranga <lilan.maduranga@gmail.com>
2018-12-23 20:05:33 +0530

    update wifi password

--- conWifi.lua
+++ conWifi.lua
@@ -2,7 +2,7 @@ local module={}
 function module.start(callback)
     -- -- One time ESP Setup --
     wifi.setmode(wifi.STATION)
-    wifi.sta.config("mad-host","madhost1")  
+    wifi.sta.config("mad-host","1234567890")  
     return callback(wifi.sta.getip())
 end
 
